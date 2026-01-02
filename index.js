e6973267613f4c344bc1b8ef1f6307ac7428d2b3
sunmeng <1105607773@qq.com>
2026-01-02 15:44:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+function sayhello(name) {
+  return `hello ${name}`
+}
+
+module.exports = {
+  sayhello
+}
+// import { withGradleProperties, type ConfigPlugin } from "@expo/config-plugins";
+// import fs from "node:fs";
+
+// interface WithGradleWrapperPluginProps {
+//   distributionUrl?: string;
+// }
+
+// const withGradleWrapperPlugin: ConfigPlugin<WithGradleWrapperPluginProps> = (
+//   expoConfig,
+//   { distributionUrl }
+// ) => {
+//   return withGradleProperties(expoConfig, (config) => {
+//     const gradleWrapperPath = `${config.modRequest.platformProjectRoot}/gradle/wrapper/gradle-wrapper.properties`;
+
+//     const properties = fs.readFileSync(gradleWrapperPath, "utf8");
+//     if (distributionUrl) {
+//       // 修改 Gradle 版本
+//       const updated = properties.replace(
+//         /distributionUrl=.+/,
+//         `distributionUrl=${distributionUrl}`
+//       );
+
+//       fs.writeFileSync(gradleWrapperPath, updated);
+//     }
+
+//     return config;
+//   });
+// };
+
+// export default withGradleWrapperPlugin;
